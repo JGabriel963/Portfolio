4d542607713024ea3394658c14fe6da0c126e4d1
João Gabriel <joaogabriel9633@gmail.com>
2023-07-26 15:53:59 -0300

    Modal Done

--- src/components/Works/Card.jsx
+++ src/components/Works/Card.jsx
@@ -20,12 +20,12 @@ export default function Card({ img, title, deploy, github }) {
           alt="project"
           className=" group-hover:scale-125 transition-all duration-500"
         />
-        <div className="absolute -bottom-full left-2 md:left-12 group-hover:bottom-20 md:group-hover:bottom-24 transition-all duration-500 z-50">
+        <div className="absolute -bottom-full left-4 md:left-12 group-hover:bottom-20 md:group-hover:bottom-24 transition-all duration-500 z-50">
           <span className="text-2xl text-primary-color font-extrabold">
             {title}
           </span>
         </div>
-        <div className="absolute -bottom-full left-2 md:left-12 group-hover:bottom-7 md:group-hover:bottom-10 transition-all duration-700 z-50 flex gap-4">
+        <div className="absolute -bottom-full left-4 md:left-12 group-hover:bottom-7 md:group-hover:bottom-10 transition-all duration-700 z-50 flex gap-4">
           <a
             href={deploy}
             target="_blank"
@@ -53,7 +53,7 @@ export default function Card({ img, title, deploy, github }) {
           </button>
         </div>
       </div>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Modal isOpen={isOpen} img={img} title={title} setIsOpen={setIsOpen} />
     </div>
   );
 }
--- src/components/Works/Modal.jsx
+++ src/components/Works/Modal.jsx
@@ -1,13 +1,34 @@
-export default function Modal({ isOpen, setIsOpen }) {
+import { GoIssueClosed } from 'react-icons/go'
+import { PiEyeClosedBold } from 'react-icons/pi'
+import { MdClose } from 'react-icons/md'
+
+export default function Modal({ isOpen, setIsOpen, img, title }) {
     if (isOpen) {
         return(
             <div className="fixed top-0 bottom-0 left-0 right-0 bg-tertiary-color/50 z-50">
                 <div className="h-full w-full flex justify-center items-center">
                     <div 
-                        className="flex flex-col items-center  bg-background p-6 rounded-2xl w-[90%] md:w-[60%] relative"
+                        className="flex flex-col  bg-background p-6 pb-9 rounded-2xl w-[90%] md:w-[60%] relative"
                     >
-                        
-                        <button onClick={() => setIsOpen(!isOpen)}>CloseOpen</button>
+                        <img 
+                            src={img} 
+                            alt="project"
+                            className="rounded-2xl" 
+                        />
+
+                        <h1 className="font-extrabold text-3xl my-2 text-tertiary-color">{title}</h1>
+
+                        <p
+                            className="p-1 text-gray-2"
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur pariatur natus cum assumenda aspernatur a totam? At expedita excepturi ex, odio, maiores cupiditate doloremque nulla recusandae tenetur illo natus enim quidem quaerat rem. Eius neque dolores, iste atque, unde maiores hic mollitia eos dolor distinctio minus! Vel voluptatum natus sint.
+                        </p>
+                        <button
+                            className="absolute bottom-3 right-3 bg-tertiary-color text-white p-2 rounded-full text-2xl"
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            <MdClose />
+                        </button>
                     </div>
                 </div>
             </div>
