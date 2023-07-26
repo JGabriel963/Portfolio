2b19275a76bbff59759f495a5976b3663267e511
João Gabriel <joaogabriel9633@gmail.com>
2023-07-26 15:30:43 -0300

    Works responsive

--- src/components/Works/Card.jsx
+++ src/components/Works/Card.jsx
@@ -20,19 +20,19 @@ export default function Card({ img, title, deploy, github }) {
           alt="project"
           className=" group-hover:scale-125 transition-all duration-500"
         />
-        <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+        <div className="absolute -bottom-full left-2 md:left-12 group-hover:bottom-20 md:group-hover:bottom-24 transition-all duration-500 z-50">
           <span className="text-2xl text-primary-color font-extrabold">
             {title}
           </span>
         </div>
-        <div className="absolute -bottom-full left-12 group-hover:bottom-10 transition-all duration-700 z-50 flex gap-4">
+        <div className="absolute -bottom-full left-2 md:left-12 group-hover:bottom-7 md:group-hover:bottom-10 transition-all duration-700 z-50 flex gap-4">
           <a
             href={deploy}
             target="_blank"
             className="border border-white bottom-2 flex items-center gap-1 px-2 py-2 rounded-md
                 hover:border-primary-color hover:bg-primary-color hover:text-black transition-colors"
           >
-            View <FiExternalLink />
+            <p className="text-sm md:text-base">View</p> <FiExternalLink />
           </a>
           <a
             href={github}
@@ -40,14 +40,16 @@ export default function Card({ img, title, deploy, github }) {
             className="border border-white bottom-2 flex items-center gap-1 px-2 py-2 rounded-md
                 hover:border-primary-color hover:bg-primary-color hover:text-black transition-colors"
           >
-            GitHub <BsGithub />
+            <p className="text-sm md:text-base">GitHub</p> 
+            <BsGithub />
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
             className="border border-white bottom-2 flex items-center gap-1 px-2 py-2 rounded-md
                 hover:border-primary-color hover:bg-primary-color hover:text-black transition-colors"
           >
-            About <CgDetailsMore />
+            <p className="text-sm md:text-base">About</p>  
+            <CgDetailsMore />
           </button>
         </div>
       </div>
--- src/components/Works/Modal.jsx
+++ src/components/Works/Modal.jsx
@@ -3,9 +3,10 @@ export default function Modal({ isOpen, setIsOpen }) {
         return(
             <div className="fixed top-0 bottom-0 left-0 right-0 bg-tertiary-color/50 z-50">
                 <div className="h-full w-full flex justify-center items-center">
-                    <div>
-                        Modal
-                        <br />
+                    <div 
+                        className="flex flex-col items-center  bg-background p-6 rounded-2xl w-[90%] md:w-[60%] relative"
+                    >
+                        
                         <button onClick={() => setIsOpen(!isOpen)}>CloseOpen</button>
                     </div>
                 </div>
