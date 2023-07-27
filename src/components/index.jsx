d75136c0db4623678f711f79d238c5934d4a711b
João Gabriel <joaogabriel9633@gmail.com>
2023-07-26 22:32:16 -0300

    Update Title and Add propety select-none in component Container

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="relative font-poppins bg-background box-border ">
+    <div className="relative font-poppins bg-background box-border select-none">
       {children}
     </div>
   );
