b78504b224c7e6cae60449d8ca3e47379d830493
João Gabriel <joaogabriel9633@gmail.com>
2023-09-19 22:15:22 -0300

    Add news skills

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -16,48 +16,74 @@ const skills = [
   },
   {
     id: 4,
+    icon: "devicon-typescript-plain",
+    name: "Typescript"
+  },
+  {
+    id: 5,
     icon: "devicon-python-plain",
     name: "PYTHON"
   },
   {
-    id: 5,
+    id: 6,
     icon: "devicon-git-plain",
     name: "Git"
   },
   {
-    id: 6,
+    id: 7,
     icon: "devicon-react-original",
     name: "React JS"
   },
   {
-    id: 7,
+    id: 8,
+    icon: "devicon-nextjs-original",
+    name: "Next JS"
+  },
+  {
+    id: 9,
     icon: "devicon-bootstrap-plain",
     name: "Bootstrap"
   },
   {
-    id: 8,
+    id: 10,
     icon: "devicon-tailwindcss-plain",
     name: "Tailwind CSS"
   },
   {
-    id: 9,
+    id: 11,
     icon: "devicon-sass-original",
     name: "Sass"
   },
   {
-    id: 10,
+    id: 12,
     icon: "devicon-nodejs-plain",
     name: "Node JS"
   },
   {
-    id: 11,
+    id: 13,
     icon: "devicon-mongodb-plain",
     name: "MongoDB"
   },
   {
-    id: 12,
+    id: 14,
     icon: "devicon-postgresql-plain",
     name: "PostgresSQL"
+  },
+  {
+    id: 15,
+    icon: "devicon-express-original",
+    name: "Express"
+  }
+  ,
+  {
+    id: 15,
+    icon: "devicon-firebase-plain",
+    name: "Firebase"
+  },
+  {
+    id: 17,
+    icon: "devicon-fastapi-plain",
+    name: "Fastapi"
   }
 ]
 
